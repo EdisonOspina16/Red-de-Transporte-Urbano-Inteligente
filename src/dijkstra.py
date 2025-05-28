869fef4c332d4d70d26174f692eec4f9ec795204
JHONCE79 <jhonsteven.163@gmail.com>
2025-05-27 23:24:39 -0500

    Docstring dijkstra

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -2,6 +2,19 @@ import heapq
 
 
 def dijkstra(grafo, inicio):
+    """
+    Implementa el algoritmo de Dijkstra para encontrar el camino más corto desde un nodo inicial
+    a todos los demás nodos en el grafo.
+    
+    Args:
+        grafo (Grafo): Grafo que representa la red de transporte
+        inicio (str): ID del nodo inicial
+        
+    Returns:
+        tuple: (distancias, camino) donde:
+            - distancias (dict): Diccionario con las distancias mínimas desde el inicio a cada nodo
+            - camino (dict): Diccionario con los caminos mínimos desde el inicio a cada nodo
+    """
     distancias = {v: float('inf') for v in grafo.vertices}
     distancias[inicio] = 0
     camino = {v: [] for v in grafo.vertices}
